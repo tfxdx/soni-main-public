fde1442b1948c475aa3729c239166c325c10501e
tfxdx <yunumendozaperez@gmail.com>
2026-04-07 16:41:13 -0500

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,1 +1,13 @@
 import Paths;
+
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end

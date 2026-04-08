ffa6e796ce17601d747b62cfe6e942e451a0662c
tfxdx <yunumendozaperez@gmail.com>
2026-04-08 13:26:36 -0500

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -132,18 +132,6 @@ class PauseSubState extends MusicBeatSubstate
 
 		regenMenu();
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
-
-                #if android
-                if (PlayState.chartingMode)
-                {
-                        addVirtualPad(FULL, A);
-                }
-                else
-                {
-                        addVirtualPad(UP_DOWN, A);
-                }
-                addPadCamera();
-                #end
 	}
 
 	var holdTime:Float = 0;
